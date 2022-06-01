eeaed1ff8d954db321b1b5aaee3a7f9d4cc48af3
READYonHub <104925582+READYonHub@users.noreply.github.com>
2022-06-01 11:24:49 +0200

    Update and rename c++_works/keresett karakter ascii száma.cpp to c++_működik/keresett karakter ascii száma.cpp

---
+++ c++_működik/keresett karakter ascii száma.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    //az aposztrofok közé írd be a keresett karakteredet és megmondja az ascii számát
+	char character = 'a';
+	int integer = character;
+	cout << "A keresett karakarter: \t[--- " << character << " ---]\nASCII kódja a ---> \t[--- "
+	<< static_cast<int>(character)<< " ---]";
+}
