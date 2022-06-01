baee497d9d171ee83db701c1091de470124fd103
READYonHub <104925582+READYonHub@users.noreply.github.com>
2022-06-01 11:08:56 +0200

    Create keresett karakter ascii száma.cpp

---
+++ c++_works/keresett karakter ascii száma.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    //az aposztrofok közé írd be a keresett karakteredet és megmondja az ascii számát
+	char character = 'x';
+	int integer = character;
+	cout << character << " " << static_cast<int>(character);
+	cout << " " << integer << " " << static_cast<char>(integer);
+}
