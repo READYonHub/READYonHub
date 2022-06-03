cab99f50346460b6b0f9978c7e7fff32728cbf27
READYonHub <104925582+READYonHub@users.noreply.github.com>
2022-06-03 12:28:46 +0200

    Add files via upload

---
+++ c#_works/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace kiíírja_a_számok_összegét_és_a_karaktereket_a_tömbből
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] values = { "12,3", "45", "ABC", "11", "DEF" };
+
+            decimal total = 0m;
+            string message = "";
+
+            foreach (var value in values)
+            {
+                decimal number;
+                if (decimal.TryParse(value, out number))
+                {
+                    total += number;
+                }
+                else
+                {
+                    message += value;
+                }
+            }
+
+            Console.WriteLine($"Message: {message}");
+            Console.WriteLine($"Total: {total}");
+            Console.ReadLine();
+        }
+    }
+}
